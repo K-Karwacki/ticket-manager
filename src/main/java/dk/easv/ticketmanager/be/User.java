f19648e292ba67ae7707226425d67a047fcd1f62
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-03-19 16:45:40 +0100

    Assigning coordinators to events
    # Conflicts:
    #	src/main/java/dk/easv/ticketmanager/be/User.java

--- src/main/java/dk/easv/ticketmanager/be/User.java
+++ src/main/java/dk/easv/ticketmanager/be/User.java
@@ -3,6 +3,9 @@ package dk.easv.ticketmanager.be;
 import jakarta.persistence.*;
 import javafx.scene.image.Image;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "[User]")
 public class User {
@@ -11,16 +14,13 @@ public class User {
     private long id;
 
     @Column(name = "first_name")
-    private String first_name;
+    private String firstName;
 
     @Column(name = "last_name")
-    private String last_name;
+    private String lastName;
 
     @Column(name = "role_id")
     private long role_id;
-
-    @Column(name = "phone_number")
-    private String phone_number;
 
     @Column(name = "email")
     private String email;
@@ -28,90 +28,86 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
     @Column(name = "image_path")
     private String imagePath;
 
+    @ManyToMany(mappedBy = "coordinators")
+    private Set<Event> coordinatedEvents = new HashSet<>();
+
     // Default constructor
     public User() {
-        this.id = 0;
-        this.first_name = "Ronaldihno";
-        this.last_name = "Gaucho";
-        this.email = "ronaldihnogaucho123@gmail.com";
-        this.password = "password";
-        this.imagePath = "images/event-template.jpg"; // Default image path
+        // Minimal defaults
     }
 
     // Parameterized constructor
-    public User(int id, String first_name,String last_name, String email, String password, String phone_number, String imagePath, long role_id) {
+    public User(long id, String firstName, String lastName, String email, String password, String phoneNumber, String imagePath, long role_id) {
         this.id = id;
-        this.first_name = first_name;
-        this.last_name = last_name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
-        this.phone_number = phone_number;
         this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.imagePath = imagePath;
         this.role_id = role_id;
-//        this.imagePath = imagePath;
     }
 
-    // Getters and Setters
+    // Getters and setters (updated naming)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getRoleId() {
+        return role_id;
+    }
+
+    public void setRoleId(long roleId) {
+        this.role_id = roleId;
+    }
+
+    // Other getters/setters unchanged
+
+    public Set<Event> getCoordinatedEvents() {
+        return coordinatedEvents;
+    }
+
+    public void setCoordinatedEvents(Set<Event> coordinatedEvents) {
+        this.coordinatedEvents = coordinatedEvents;
+    }
+
+    public Image getUserImage() {
+        try {
+            return new Image(imagePath);
+        } catch (Exception e) {
+            return new Image("images/event-template.jpg"); // Fallback image
+        }
+    }
+
     public long getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getFirst_name() {
-        return first_name;
+        return firstName;
     }
     public String getLast_name() {
-        return last_name;
+        return lastName;
     }
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public String getPhone_number() { return phone_number;}
-    public void setPhone_number(String phone_number) {this.phone_number = phone_number;}
-
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    // Method to get the JavaFX Image from the image path (similar to Event)
-    public Image getUserImage() {
-        return new Image(imagePath);
-    }
-
-    @Override
-    public String toString() {
-        return first_name + " " + last_name;
+    public void assignCoordinator(Event event) {
+        this.coordinatedEvents.add(event);
     }
 }
