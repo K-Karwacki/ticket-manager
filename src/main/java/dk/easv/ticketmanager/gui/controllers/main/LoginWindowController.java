2da363299b32df28537bf8dad691d17f02249682
pieeernik <igopie01@easv365.dk>
2025-03-24 22:27:42 +0100

    delete user + popups + fixed bugs
    # Conflicts:
    #	src/main/java/dk/easv/ticketmanager/be/Location.java
    #	src/main/java/dk/easv/ticketmanager/dal/implementations/TicketRepository.java
    #	src/main/java/dk/easv/ticketmanager/gui/FXMLPath.java
    
    # Conflicts:
    #	src/main/java/dk/easv/ticketmanager/gui/controllers/dashboards/UserListDashboardController.java
    #	src/main/java/dk/easv/ticketmanager/gui/controllers/main/LoginWindowController.java
    #	src/main/java/dk/easv/ticketmanager/gui/controllers/popups/UserFormPopupController.java
    #	src/main/resources/dk/easv/ticketmanager/fxml/popups/user_form_popup.fxml

--- src/main/java/dk/easv/ticketmanager/gui/controllers/main/LoginWindowController.java
+++ src/main/java/dk/easv/ticketmanager/gui/controllers/main/LoginWindowController.java
@@ -64,7 +64,7 @@ public class LoginWindowController
             User authenticatedUser = authenticationService.authenticateUser(inputUsername, inputPassword);
             if (authenticatedUser != null) {
                 userSession.setUser(authenticatedUser);
-                goToMainPage();
+                goToMainPage(actionEvent);
             } else {
                 errorLabel.setText("Invalid username or password");
             }
@@ -88,7 +88,7 @@ public class LoginWindowController
         });
     }
 
-  private void goToMainPage() throws IOException {
+  private void goToMainPage(ActionEvent event) throws IOException {
 
       FXMLLoader loader = new FXMLLoader(getClass().getResource("/dk/easv/ticketmanager/fxml/main/main.fxml"));
       Parent root = loader.load();
@@ -99,7 +99,7 @@ public class LoginWindowController
       stage.setMaximized(false);
       stage.show();
 
-      Stage currentStage = (Stage) textFieldPassword.getScene().getWindow();
+      Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
       currentStage.close();
   }
 
