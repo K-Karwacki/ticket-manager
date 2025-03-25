45966eb8ad4469e1c7a8cbcf172759773638faa4
pieeernik <igopie01@easv365.dk>
2025-03-25 10:41:06 +0100

    editing users

--- src/main/java/dk/easv/ticketmanager/be/User.java
+++ src/main/java/dk/easv/ticketmanager/be/User.java
@@ -70,7 +70,6 @@ public class User {
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -78,7 +77,6 @@ public class User {
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -86,10 +84,23 @@ public class User {
     public Role getRole() {
         return role;
     }
-
     public void setRole(Role role) {
         this.role = role;
     }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {this.password = password;}
+
+    public String getImagePath() {return imagePath;}
+    public void setImagePath(String imagePath) {this.imagePath = imagePath;}
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {this.email = email;}
+
 
     // Other getters/setters unchanged
 
@@ -119,6 +130,9 @@ public class User {
     public String getLast_name() {
         return lastName;
     }
+    public String getPhoneNumber() { return phoneNumber;}
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
+
 
     public void assignEventToCoordinator(Event event) {
         this.coordinatedEvents.add(event);
@@ -129,13 +143,4 @@ public class User {
         return firstName + " " + lastName + " " + role.toString();
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhoneNumber() {return phoneNumber;}
 }
