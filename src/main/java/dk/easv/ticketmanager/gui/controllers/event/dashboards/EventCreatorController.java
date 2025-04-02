05833ed1b15ec9a9c60951b776187ef08ba79b90
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-04-02 13:40:16 +0200

    add event

--- src/main/java/dk/easv/ticketmanager/gui/controllers/event/dashboards/EventCreatorController.java
+++ src/main/java/dk/easv/ticketmanager/gui/controllers/event/dashboards/EventCreatorController.java
@@ -2,9 +2,10 @@ package dk.easv.ticketmanager.gui.controllers.event.dashboards;
 
 import dk.easv.ticketmanager.be.Event;
 import dk.easv.ticketmanager.be.Location;
+
 import dk.easv.ticketmanager.bll.services.interfaces.EventManagmentService;
 import dk.easv.ticketmanager.gui.FXMLManager;
-import dk.easv.ticketmanager.gui.controllers.popups.ImageSelectorPopupController;
+import dk.easv.ticketmanager.gui.ViewManager;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,18 +16,15 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.util.Pair;
-
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
+import static dk.easv.ticketmanager.gui.FXMLPath.EVENTS_DASHBOARD;
 import static dk.easv.ticketmanager.gui.FXMLPath.IMAGE_SELECTOR_POPUP;
 
-public class EventCreatorController
-{
-    private EventManagmentService eventManagmentService;
-    private final FXMLManager fxmlManager = FXMLManager.INSTANCE;
+public class EventCreatorController {
+    private Event event = new Event();
     private String imagePath;
+    private static EventManagmentService eventManagmentService;
 
     @FXML
     private ImageView imageViewSelectedImage;
@@ -47,58 +45,42 @@ public class EventCreatorController
     @FXML
     private TextField txtFieldEventPostalCode;
 
-    public EventCreatorController(){
-
+    @FXML private void addEvent(){
+        setEventData();
+        Stage stage = (Stage) txtFieldEventName.getScene().getWindow();
+        stage.close();
+        eventManagmentService.createNewEvent(event);
+        Pair<Parent, EventHomeController> p = FXMLManager.INSTANCE.getFXML(EVENTS_DASHBOARD);
+        p.getValue().loadEventCards();
     }
 
-    @FXML private void onClickAddNewEvent(){
-
-        if(txtFieldEventLocationName.getText().isEmpty()){
-            txtFieldEventLocationName.setStyle("-fx-border-color: red;");
-            return;
-        }
-
-        // Assume that all fields are not null
-
+    private void setEventData(){
         Location location = new Location();
         location.setName(txtFieldEventLocationName.getText());
         location.setCity(txtFieldEventCity.getText());
         location.setPostCode(txtFieldEventPostalCode.getText());
         location.setAddress(txtFieldEventAddress.getText());
 
-        Event newEvent = new Event();
-        newEvent.setLocation(location);
-        newEvent.setImagePath(imagePath);
-        newEvent.setName(txtFieldEventName.getText());
-        newEvent.setDescription(txtAreaEventDescription.getText());
-        newEvent.setDate(datePickerEventDate.getValue());
-        newEvent.setTime(txtFieldEventTime.getText());
 
-        Stage stage = (Stage) txtFieldEventName.getScene().getWindow();
-        stage.close();
-//        eventDataModel.addEvent(event);
-//        eventDataModel.loadEvents();
-//        Pair<Parent, EventListDashboardController> p = fxmlManager.getFXML(EVENTS_DASHBOARD);
-//        p.getValue().load();
+        event.setLocation(location);
+        event.setImagePath(imagePath);
+        event.setName(txtFieldEventName.getText());
+        event.setDescription(txtAreaEventDescription.getText());
+        event.setDate(datePickerEventDate.getValue());
+        event.setTime(txtFieldEventTime.getText());
+        event.setDate(datePickerEventDate.getValue());
     }
 
 
-
     @FXML private void chooseImage() {
-        Pair<Parent, ImageSelectorPopupController> p = fxmlManager.getFXML(IMAGE_SELECTOR_POPUP);
-        Stage stage = new Stage();
-        stage.setTitle("Image Selector");
-        stage.setScene(new Scene(p.getKey()));
-        stage.show();
+        ViewManager.INSTANCE.showPopup(IMAGE_SELECTOR_POPUP, "Image Selector");
     }
 
     public void setImage(String imagePath) {
         this.imagePath = imagePath;
         imageViewSelectedImage.setImage(new Image(Objects.requireNonNull(getClass().getResource(imagePath)).toExternalForm()));
     }
-
-    public void setDatabaseService(EventManagmentService eventManagmentService)
-    {
+    public void setDatabaseService(EventManagmentService eventManagmentService) {
         this.eventManagmentService = eventManagmentService;
     }
 }
